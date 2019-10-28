806b4389a0d802d4e3c3fff3c49eab915f06a970
Ashok Dey <ashokdey100@gmail.com>
2019-10-29 01:07:07 +0530

    update: override what() in derived std::exception

--- IndexException.cc
+++ IndexException.cc
@@ -10,8 +10,11 @@ public:
   ArrayIndexException() = default;
   explicit ArrayIndexException(int index)
   {
-    std::cout << m_message << "\nYou tried to access index ->"
-              << index << " which is invalid\n"
-              << std::endl;
+    m_message += "\nYou tried to access index ->" + std::to_string(index) + " which is invalid\n";
+  }
+
+  virtual const char *what() const noexcept
+  {
+    return m_message.c_str();
   }
 };
--- main.cc
+++ main.cc
@@ -4,23 +4,34 @@
 
 int main(int argc, char **argv)
 {
-  std::cout << "Creating an empty array" << std::endl;
+  try
+  {
+    std::cout << "Creating an empty array" << std::endl;
 
-  IntArray a{};
-  std::cout << "Size of a = " << a.size() << std::endl;
-  assert(a.isEmpty());
+    IntArray a{};
+    std::cout << "Size of a = " << a.size() << std::endl;
+    assert(a.isEmpty());
 
-  std::cout << "---------------------------------------" << std::endl;
+    std::cout << "---------------------------------------" << std::endl;
 
-  std::cout << "Creating and array with 10 elements" << std::endl;
-  IntArray b{10};
-  std::cout << "Size of b = " << b.size() << std::endl;
-  assert(!b.isEmpty());
+    std::cout << "Creating and array with 10 elements" << std::endl;
+    IntArray b{10};
+    std::cout << "Size of b = " << b.size() << std::endl;
+    assert(!b.isEmpty());
 
-  b[1] = 30;
+    b[1] = 30;
 
-  std::cout << "b[1] = " << b[1] << std::endl;
-  std::cout << "Tring to access b[13] = " << b[13] << std::endl;
+    std::cout << "b[1] = " << b[1] << std::endl;
+    std::cout << "Tring to access b[13] = " << b[13] << std::endl;
 
-  return 0;
+    return 0;
+  }
+  catch (const std::exception &e)
+  {
+    std::cout << "\n***************************************" << std::endl;
+    std::cout << "Sorry, the program crashed :(" << std::endl;
+    std::cout << "--Error Report--" << std::endl;
+    std::cerr << e.what() << std::endl;
+    return -1;
+  }
 }
